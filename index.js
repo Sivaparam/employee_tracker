75ab633ed6e0193076fff304155a291f310cb44a
Sivaranjani Jayaprakash <shiva.ranjani83@gmail.com>
2022-07-27 21:48:27 -0400

    add employee, role, department logic added

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 //packages
 const fs = require('fs');
 const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const ctable = require('console.table');
 
 //import class and prompts
-const {Department, depPrompts} = require('./lib/department');
-const {Employee,empPrompts} = require('./lib/employee');
-const {Role, rolePrompts} = require('./lib/role');
+const { Department, depPrompts } = require('./lib/department');
+const { Employee, empPrompts } = require('./lib/employee');
+const { Role, rolePrompts } = require('./lib/role');
+const Connection = require('mysql2/typings/mysql/lib/Connection');
 
+const Connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'employee_db'
+});
 
 //Inquirer for options
 const options = [
@@ -26,36 +34,36 @@ async function displayoptions() {
     console.log('Welcome to Employee Tracker');
 
     await inquirer.prompt(options)
-    .then(data => {
-        switch (data.option_selected) {
-            case "View All Employees":
-                getEmployeeData();
-                break;
-            case "Add Employee":
-                addEmployee();
-                break;
-            case "Update Employee Role":
-                updateEmpRole();
-                break;
-            case "View All Roles":
-                getRoles();
-                break;
-            case "Add Role":
-                addRole();
-                break;
-            case "View All Departments":
-                getDepartment();
-                break;
-            case "Add Department":
-                addDepartment();
-                break;
-            case "Quit":
-                break;
-        }
-    })
-    .catch(error => {
-        console.log(`Error occurred in options: ${error}`);
-    })
+        .then(data => {
+            switch (data.option_selected) {
+                case "View All Employees":
+                    getEmployeeData();
+                    break;
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Update Employee Role":
+                    updateEmpRole();
+                    break;
+                case "View All Roles":
+                    getRoles();
+                    break;
+                case "Add Role":
+                    addRole();
+                    break;
+                case "View All Departments":
+                    getDepartment();
+                    break;
+                case "Add Department":
+                    addDepartment();
+                    break;
+                case "Quit":
+                    break;
+            }
+        })
+        .catch(error => {
+            console.log(`Error occurred in options: ${error}`);
+        })
 };
 
 //retrieves all employee details from employee table
@@ -64,12 +72,22 @@ function getEmployeeData() {
 };
 
 //adds a new employee to employee table
-function addEmployee() {
-
+async function addEmployee() {
+    await inquirer.prompt(empPrompts)
+        .then(data => {
+            const employee = new Employee(data.firstName, data.lastName);
+            const sql = employee.addEmployee();
+            Connection.createQuery(sql, [data.firstName, data.lastName], (err, result) => {
+                err ? console.log(err) : console.log(`Added ${data.firstName} ${data.lastName} to employee table`)
+            })
+        })
+        .catch(error => {
+            console.log(`Error occurred in add Employee ${error}`);
+        })
 };
 
 //updates role of employee  to employee table
-function updateEmpRole(){
+function updateEmpRole() {
 
 };
 
@@ -79,13 +97,37 @@ function getRoles() {
 };
 
 //adds a new role to role table
-function addRole() {
-
+async function addRole() {
+    await inquirer.prompt(rolePrompts)
+        .then(data => {
+            
+            const role = new Role(data.title, data.salary);
+            const sql = role.addRole();
+            Connection.createQuery(sql, [data.title, data.salary], (err, result) => {
+                err ? console.log(err) : console.log(`Added ${data.title} to Role table`);
+            })
+        })
+        .catch(error => {
+            console.log(`Error occurred during add role ${error}`);
+        })
 };
 
 //adds a new department to department table
-function addDepartment() {
-
+async function addDepartment() {
+    await inquirer.prompt(depPrompts)
+        .then(data => {
+            const department = new Department(data.depName);
+            //  var departmentData = department.addDepartment();
+            // fs.appendFile(depFile, departmentData, (err) => 
+            //err? console.log(err): console.log('Data added to Departmemt Table'));
+            const sql = department.addDepartment();
+            Connection.createQuery(sql, data.depName, (err, result) => {
+                err ? console.log(err) : console.log(`Added ${data.depName} to department table`);
+            })
+        })
+        .catch(error => {
+            console.log(`Error occurred during add department ${error}`);
+        })
 };
 
 //retrieves all department from department table
